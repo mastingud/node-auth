f7ac77a770ab9f0c33555542d8d627b0ad023e37
mastingud <63357619+mastingud@users.noreply.github.com>
2020-06-26 10:34:16 +0700

    gsnti cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 var corsOptions = {
   // origin: "https://article-irfan.herokuapp.com",
-  origin :"http://recondite-notebook.surge.sh"
+  origin :"https://frantic-joke.surge.sh"
 };
 
 
